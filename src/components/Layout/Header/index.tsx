4ff5f97a2a4f8860e7ab14a00322871dbff91b36
divitegar <divitegarh@gmail.com>
2021-09-20 17:51:39 +0700

    search component

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,18 +1,28 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import Container from 'components/Container'
-import  traveloLogo from 'public/assets/travelo-logo-header.png'
+import Container from "components/Container";
+import Search from "components/Search";
+import traveloLogo from "public/assets/travelo-logo-header.png";
 
 const Header = () => {
-    return (
-        <header className="h-[85px] bg-white shadow-lg p-4">
-            <Container>
-            <div className="flex justify-between item-center">
-                <Image src={traveloLogo} alt="travelo logo" height="48" width="182" placeholder="blur"/>
-            </div>
-            </Container>
-        </header>
-    )
-}
+  return (
+    <header className="h-[85px] bg-white shadow-lg p-4">
+      <Container>
+        <div className="flex justify-between item-center">
+          <div className="flex">
+            <Image
+              src={traveloLogo}
+              alt="travelo logo"
+              height="48"
+              width="182"
+              placeholder="blur"
+            />
+            <Search />
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
