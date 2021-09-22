5caa1d2807f4b8cfbaf28ab324e9ebaeb83c1fd5
divitegar <divitegarh@gmail.com>
2021-09-22 17:24:43 +0700

    component footer info

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -1,4 +1,5 @@
 import Container from "components/Container";
+import FooterInfo from "./FooterInfo"
 import styles from "./Footer.module.css";
 import Image from "next/image";
 import traveloLogo from "public/assets/travelo-logo-footer.png";
@@ -17,6 +18,58 @@ const Footer = () => {
               width="136"
             />
           </figure>
+          <div className="flex flex-1 justify-between ml-[106px]">
+            <FooterInfo
+              title="Info"
+              content={[
+                {
+                  label: 'Tentang kami',
+                  href: '/'
+                },
+                {
+                  label: 'Testimonial',
+                  href: '/'
+                },
+                {
+                  label: 'Kontak',
+                  href: '/'
+                },
+              ]}
+            />
+
+            <FooterInfo
+              title="Perusahaan"
+              content={[
+                {
+                  label: 'Syarat & Ketentuan',
+                  href: '/'
+                },
+                {
+                  label: 'Kebijakan privasi',
+                  href: '/'
+                },
+                {
+                  label: 'Blog',
+                  href: '/'
+                },
+                {
+                  label: 'Bantuan pelayanan',
+                  href: '/'
+                },
+              ]}
+            />
+
+            <FooterInfo
+              title="Kontak"
+              content={[
+                'Jl. Dago No. 1A',
+                'Bandung, Jawa Barat',
+                'Indonesia',
+                '(022)8080808',
+                'contact@email.com'
+              ]}
+            />
+          </div>
         </div>
       </Container>
 
