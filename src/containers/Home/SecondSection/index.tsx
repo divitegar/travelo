8fc8bfe6a923cc0e47f1c2f1450f81862daae9e7
divitegar <divitegarh@gmail.com>
2021-10-04 22:34:11 +0700

    component promo carousel

--- src/containers/Home/SecondSection/index.tsx
+++ src/containers/Home/SecondSection/index.tsx
@@ -1,9 +1,19 @@
-import react from 'react';
+import react from "react";
+
+import Container from "components/Container";
+import PromoCarousel from "./PromoCarousel";
+
+import styles from "./SecondSection.module.css";
+import 'react-multi-carousel/lib/styles.css';
 
 const SecondSection = () => {
-    return (
-        <div></div>
-    )
-}
+  return (
+    <section className={styles.section}>
+      <Container>
+        <PromoCarousel />
+      </Container>
+    </section>
+  );
+};
 
 export default SecondSection;
