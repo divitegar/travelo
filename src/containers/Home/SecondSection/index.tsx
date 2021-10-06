fbbbecad2ce0de6c86b33f6a8207ff0c0d94a138
divitegar <divitegarh@gmail.com>
2021-10-06 09:01:25 +0700

    component testimonial

--- src/containers/Home/SecondSection/index.tsx
+++ src/containers/Home/SecondSection/index.tsx
@@ -4,6 +4,7 @@ import Container from "components/Container";
 import PromoCarousel from "./PromoCarousel";
 import Packages from "./Packages";
 import Categories from "./Categories";
+import Testimonials from "./Testimonials"
 
 import styles from "./SecondSection.module.css";
 import 'react-multi-carousel/lib/styles.css';
@@ -15,6 +16,7 @@ const SecondSection = () => {
         <PromoCarousel />
         <Packages />
         <Categories />
+        <Testimonials />
       </Container>
     </section>
   );
